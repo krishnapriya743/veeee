e6368d351558e3337f35828d5275d4887dc7ec61
krishnapriya743 <93418965+krishnapriya743@users.noreply.github.com>
2024-01-09 15:54:14 +0530

    cohere_hackaton.py

--- Cohere-Interview-Bot/cohere_hackaton.py
+++ Cohere-Interview-Bot/cohere_hackaton.py
@@ -5,7 +5,7 @@ import streamlit as st
 from streamlit_chat import message
 from amazon import set_background 
 from PyPDF2 import PdfReader
-API_KEY='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'
+API_KEY='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'
 co = cohere.Client(API_KEY)
 
 #################------SOME PAGE DESIGNING !--------###################
